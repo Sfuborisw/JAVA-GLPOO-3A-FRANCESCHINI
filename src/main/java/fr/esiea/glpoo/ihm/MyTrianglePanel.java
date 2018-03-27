109f1442c922d6042e7138bfd40eef7ea6c87a1f
JoffreyFrancesch <franceschini@et.esiea.fr>
2018-03-27 09:55:13 +0200

    Creating a random tree need to do Save as PNG

--- src/main/java/fr/esiea/glpoo/ihm/MyTrianglePanel.java
+++ src/main/java/fr/esiea/glpoo/ihm/MyTrianglePanel.java
@@ -6,25 +6,49 @@ import java.awt.Graphics2D;
 
 import javax.swing.JPanel;
 
+import org.apache.log4j.Logger;
+
+import fr.esiea.glpoo.Launcher;
+import fr.esiea.glpoo.tirage.Tirage;
+
 public class MyTrianglePanel extends JPanel {
 	private static final long serialVersionUID = 1L;
-	final int level = 10;
-
+	private final static Logger log = Logger.getLogger(Launcher.class);
+	private int red, blue, green;
+	public int level;
+	
 	public void paintComponent(Graphics g) {
-		g.setColor(Color.BLACK);
-		drawTriangle(level, getWidth() / 2, getHeight() / 2, 360, (Graphics2D) g);
+		g.setColor(new Color(red, green, blue));
+		setBackground(Color.WHITE);
+		log.debug("level = " + level);
+		log.debug("color " + red + " " + blue + " " + green);
+		drawSierpinskyTriangle(level, 500, 100, 550, (Graphics2D)g);
+	}
+	
+	
+	private static void drawSierpinskyTriangle(int level, int x, int y, int size, Graphics2D g) {
+		if(level <= 0) return;
+ 
+		g.drawLine(x, y, x+size, y);
+		g.drawLine(x, y, x, y+size);
+		g.drawLine(x+size, y, x, y+size);
+ 
+		drawSierpinskyTriangle(level-1, x, y, size/2, g);
+		drawSierpinskyTriangle(level-1, x+size/2, y, size/2, g);
+		drawSierpinskyTriangle(level-1, x, y+size/2, size/2, g);
 	}
 
-	public void drawTriangle(int level, int x, int y, int size, Graphics2D g) {
-		if (level <= 0) {
-			return;
-		}
-		g.drawLine(x, y, x + size, y);
-		g.drawLine(x, y, x, y + size);
-		g.drawLine(x + size, y, x, y + size);
+	public void setColor(Tirage tirage) {
+		red = tirage.getBoule5() * 4;
+		blue = tirage.getEtoile1() * 15;
+		green = tirage.getEtoile2() * 15;
+	}
 
-		drawTriangle(level - 1, x, y, size / 2, g);
-		drawTriangle(level - 1, x + size / 2, y, size / 2, g);
-		drawTriangle(level - 1, x, y + size / 2, size / 2, g);
+	public void setLevel(Tirage tirage) {
+		if( tirage.getEtoile1() > tirage.getEtoile2()){
+			level = tirage.getEtoile1();
+		} else {
+			level = tirage.getEtoile2();
+		}
 	}
 }
--- src/main/java/fr/esiea/glpoo/ihm/Triangle.java
+++ src/main/java/fr/esiea/glpoo/ihm/Triangle.java
@@ -1,11 +1,14 @@
 package fr.esiea.glpoo.ihm;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Frame;
+import java.awt.event.ActionEvent;
 import java.io.File;
 import java.util.List;
 import java.util.Random;
 
+import javax.swing.AbstractAction;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -24,14 +27,14 @@ public class Triangle extends JFrame {
 
 	JFrame frame;
 	JButton btn_random, btn_save;
-	
+
 	MyTrianglePanel DrawPanel;
 
 	private List<Tirage> tirages;
 	private Tirage tirageEnCours = new SimpleTirage();
 	private Random rand = new Random();
 	private boolean first = true;
-	
+
 	public Triangle() throws Exception {
 		final String fileName = "src/main/resources/my_euromillions.csv";
 		final File file = new File(fileName);
@@ -42,25 +45,68 @@ public class Triangle extends JFrame {
 		if (first == true) {
 			tirageEnCours = tirages.get(0);
 		}
-		
+
 		frame = new JFrame("Sierpinski Triangle");
-		
+
 		frame.setExtendedState(Frame.MAXIMIZED_BOTH);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		btn_random = new JButton("RANDOM");
-		btn_save = new JButton("SAVE");
-		
+
+		btn_random = new JButton(new RandomTriangel());
+		btn_save = new JButton(new SaveAsPng());
+
 		final JPanel barreButton = new JPanel();
 		barreButton.add(btn_random);
 		barreButton.add(btn_save);
-		
+
 		DrawPanel = new MyTrianglePanel();
-		
+
 		frame.getContentPane().add(barreButton, BorderLayout.SOUTH);
-		frame.getContentPane().add(DrawPanel,BorderLayout.CENTER);
-		
+		frame.getContentPane().add(DrawPanel, BorderLayout.CENTER);
+		DrawPanel.setColor(tirageEnCours);
+		DrawPanel.setBackground(Color.WHITE);
+		DrawPanel.setLevel(tirageEnCours);
+
 		frame.setVisible(true);
+	}
+
+	private class RandomTriangel extends AbstractAction {
+		private static final long serialVersionUID = 1L;
+
+		public RandomTriangel() {
+			super("New Random Triangle");
+		}
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			log.info("ici action Performed Random");
+			int value = rand.nextInt(70);
+			log.debug("random effctuer " + value);
+			tirageEnCours = tirages.get(value);
+			log.debug("test some value " + tirageEnCours.getBoule1());
+			first = false;
+			DrawPanel.removeAll();
+			DrawPanel.validate();
+			DrawPanel.setColor(tirageEnCours);
+			DrawPanel.setLevel(tirageEnCours);
+			DrawPanel.revalidate();
+			DrawPanel.repaint();
+		}
+
+	}
+
+	private class SaveAsPng extends AbstractAction {
+		private static final long serialVersionUID = 1L;
+
+		public SaveAsPng() {
+			super("Save as PNG");
+		}
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			log.info("ici action Performed Save as PNG soon...");
+
+		}
+
 	}
 
 }
